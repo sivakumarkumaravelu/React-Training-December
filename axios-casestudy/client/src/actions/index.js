4142a51ce703baea442e7ff4a1defb590d316d9b
GLOBAL\kumaras8 <sivakumar.kumaravelu@anz.com>
2017-12-04 19:30:24 +0530

    Axios middleware integration with React and Redux

---
+++ axios-casestudy/client/src/actions/index.js
@@ -0,0 +1,19 @@
+'use-strict'
+
+import axios from 'axios'
+import {TEST_ACTION} from './types'
+const API_URL = 'http://localhost:3000/api'
+export function testAction(){
+    return (dispatch)=>{
+        axios.get(`${API_URL}/helloworld`)
+        .then((response) => {
+            dispatch({
+                type:TEST_ACTION,
+                payload: response.data
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+}
